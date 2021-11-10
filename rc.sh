bbbd5e1af0f33f4f44e67a76dddc20bb7fcfe047
pingme998 <77038591+pingme998@users.noreply.github.com>
2021-11-10 17:32:52 +0530

    Create rc.sh

---
+++ rc.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curl -O 'https://raw.githubusercontent.com/pingme998/easy-rc-install/main/rclone.tar.gz'
+tar xvzf rclone.tar.gz /usr/bin/rclone
+chmod +x /usr/bin/rclone
+rclone config file
