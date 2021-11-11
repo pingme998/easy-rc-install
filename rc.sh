3a2a45bba7c136b58d6a270944a1b82d8794d823
pingme998 <77038591+pingme998@users.noreply.github.com>
2021-11-11 23:17:07 +0530

    Update rc.sh

--- rc.sh
+++ rc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 curl -O 'https://raw.githubusercontent.com/pingme998/easy-rc-install/main/rclone.tar.gz'
-tar xvzf rclone.tar.gz /usr/bin/
+tar xvzf rclone.tar.gz 
+cp rclone /usr/bin/rclone
 chmod +x /usr/bin/rclone
 rclone config file
 rm rclone.tar.gz
